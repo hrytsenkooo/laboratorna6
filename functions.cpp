e1493089e473da158e9ed234dd84cbda58d31940
hrytsenkooo <121967581+hrytsenkooo@users.noreply.github.com>
2023-06-10 20:08:09 +0300

    Create functions.cpp

---
+++ functions.cpp
@@ -0,0 +1,11 @@
+#include "funcs.h"
+
+void displayMenu() {
+    std::cout << "\n============= Menu =============" << std::endl;
+    std::cout << "1) Insert an element" << std::endl;
+    std::cout << "2) Clear the table" << std::endl;
+    std::cout << "3) Search for an element by key" << std::endl;
+    std::cout << "4) Delete an element by key" << std::endl;
+    std::cout << "0) Exit" << std::endl;
+    std::cout << "================================\n" << std::endl;
+}
